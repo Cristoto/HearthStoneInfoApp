97e3f3b658f21c153fd3ffc215cbf3be9cc6ace0
Cristo Manuel Estévez hernández <19151793+Cristoto@users.noreply.github.com>
2018-02-02 22:11:42 +0000

    Creación del menú y del splash screen de la aplicación.
    Del menú quedan todavia modificaciones

---
+++ app/src/main/java/com/hsi/hearthstoneinfo/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.hsi.hearthstoneinfo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long TIEMPO_VENTANA = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Configurar para que esté en horizontal
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //Ocultar barra de título
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //Inicio de la siguiente actividad
+                Intent intentPrincipal = new Intent()
+                        .setClass(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intentPrincipal);
+            }
+
+            //Cerramos la actividad del splash para que el usuario no vuelva a esta actividad dandole atrás
+
+            //finish();
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, TIEMPO_VENTANA);
+    }
+}
