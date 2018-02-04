81997e5d490a47ad14287a09af829cbfd1486473
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-04 17:43:50 +0000

    Añadido sonido inicial (por revisar aún por fallo, se escucha doble) y modificación del método onBack de mainActivity, para que tenga la función de home y cierre la app.

--- app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hsi.hearthstoneinfo;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -60,6 +61,8 @@ public class MainActivity extends AppCompatActivity
         homeIntent.addCategory( Intent.CATEGORY_HOME );
         homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(homeIntent);
+
+
     }
 
     @Override
