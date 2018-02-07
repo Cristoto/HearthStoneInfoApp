34e06a5c4d2f0af980e7b510930e6ee60b6f0305
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-07 22:37:32 +0000

    Añadir activity para ajustes de mazo (insertar, eliminar, modificar). Añadir activity para visualizar carta, y eliminación. Cambios varios en los layout.

---
+++ app/src/main/java/com/hsi/hearthstoneinfo/InsertarMazo.java
@@ -0,0 +1,22 @@
+package com.hsi.hearthstoneinfo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class InsertarMazo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_mazo);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
