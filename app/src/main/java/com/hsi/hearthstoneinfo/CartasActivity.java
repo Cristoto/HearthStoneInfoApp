24cf6ef1a6470dd9662da7eaea57df83883cd453
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-07 20:46:40 +0000

    Añadido función de ir a contacto desde el botón flotante

--- app/src/main/java/com/hsi/hearthstoneinfo/CartasActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/CartasActivity.java
@@ -28,8 +28,9 @@ public class CartasActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(CartasActivity.this, ContactoActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
