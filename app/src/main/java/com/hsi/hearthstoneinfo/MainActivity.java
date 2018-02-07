c342d32d56cbb8d88effd3f6c3fd7b40e04aeb14
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-07 18:24:53 +0000

    Mejor nombramiento de variables

--- app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,6 +23,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView titulo = findViewById(R.id.imagenlogo);
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -45,6 +50,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
 
     }
 
