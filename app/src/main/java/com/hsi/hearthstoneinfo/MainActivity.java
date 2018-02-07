e705b21c2d5da7ccf615f18eddfe39caea916150
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-07 20:54:02 +0000

    Quitar icono email en contacto. Añadir funcion de ir a contacto en icono. Quitar importaciones inutiles.

--- app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
@@ -1,10 +1,8 @@
 package com.hsi.hearthstoneinfo;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
