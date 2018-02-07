e187a656c9acaa4ffcbbcb7d4307e4f44951421a
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-07 20:24:45 +0000

    Eliminación de los tres puntitos en todas las activitys. Eliminación de barra superior en mainActivity (quedaba feo dos barras), pero ahora no sale el nombre de esa actividad.

--- app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/MainActivity.java
@@ -24,10 +24,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView titulo = findViewById(R.id.imagenlogo);
-
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
        // setSupportActionBar(toolbar);
@@ -36,8 +32,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ContactoActivity.class);
+                startActivity(intent);
+                finish();
 
             }
         });
@@ -73,27 +70,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
