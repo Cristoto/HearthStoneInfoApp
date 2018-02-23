1153d3ee16f82df9d36a181103bb5e7d06673f33
xibhu <35465641+Xibhu@users.noreply.github.com>
2018-02-23 19:36:40 +0000

    Hecho Mazos activity. Comentado todo el código, y no hace falta hacer comprobaciones, el código reacciona automáticamente a los cambios.

--- app/src/main/java/com/hsi/hearthstoneinfo/MazosActivity.java
+++ app/src/main/java/com/hsi/hearthstoneinfo/MazosActivity.java
@@ -2,6 +2,7 @@ package com.hsi.hearthstoneinfo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,7 +16,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.hsi.hearthstoneinfo.BD.ConnSQLiteHelper;
 import com.hsi.hearthstoneinfo.Entidades.Carta;
@@ -26,14 +26,14 @@ import java.util.ArrayList;
 public class MazosActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    /*Variables que se necesitan para rellenar el spinner de mazos*/
     Spinner mazosSpinner;
-
     ArrayList<Mazo> mazoSpinnerArray;
     ArrayAdapter<Mazo> mazoSpinnerAdapter;
 
 
+    /*Variables que se necesitan para rellenar la ListView de cartas*/
     ListView mazoListView;
-
     ArrayList<Carta> mazoListArray;
     ArrayAdapter<Carta> mazoAdapterListView;
 
@@ -41,10 +41,10 @@ public class MazosActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mazos);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab =findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,30 +54,34 @@ public class MazosActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Obntener la referencia de la interfaz.
         mazosSpinner = findViewById(R.id.mazosSpinner);
         mazoListView = findViewById(R.id.mazosListView);
 
+        //Inicializar los componentes necesarios.
         mazoSpinnerArray = new ArrayList<>();
         mazoListArray = new ArrayList<>();
 
         actualizarSpinner();
 
+        /**
+         * Listener para el evento de item seleccionado en el spinner.
+         */
         mazosSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            //Cuando se selecciona un elemento, se crea un objeto mazo y se envia a la función, para que rellene el ListView.
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
                 Mazo m = (Mazo)adapterView.getItemAtPosition(i);
                 rellenarList(m);
-
             }
 
             @Override
@@ -85,8 +89,11 @@ public class MazosActivity extends AppCompatActivity
 
             }
         });
-
+        /**
+         * Listener para el ListView, para cuando se selecciona un item.
+         */
         mazoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //Se crea una carta y se envía a la función verCarta.
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
@@ -99,56 +106,78 @@ public class MazosActivity extends AppCompatActivity
 
     }
 
+    /**
+     *
+     Cuando se vuelve a esta actividad, se actualiza el spinner, para que se ponga en funcionamiento el listener, y muestre las cartas. (
+     Y en caso de que el primer mazo de cartas haya recibido cambios, se actualiza la lista de cartas)
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
         actualizarSpinner();
     }
 
+    /**
+     *
+     Recoge una carta e inicia la actividad donde se ve toda la información sobre la carta.
+     */
     public void verCarta(Carta c){
         Intent i = new Intent(this, VerCarta.class);
         i.putExtra("carta", c);
         startActivity(i);
     }
 
-
+    /**
+     *Actualiza la liste del spinner con los mazos actuales.
+     */
     public void actualizarSpinner(){
 
         ConnSQLiteHelper c = new ConnSQLiteHelper(this);
         mazoSpinnerArray = c.consultarTodosMazos();
-        mazoSpinnerAdapter = new ArrayAdapter<Mazo>(this, R.layout.support_simple_spinner_dropdown_item, mazoSpinnerArray);
+        mazoSpinnerAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, mazoSpinnerArray);
         mazosSpinner.setAdapter(mazoSpinnerAdapter);
-
 
     }
 
+    /**
+     * Rellena la ListView con el mazo seleccionado
+     * @param m Mazo del que se quiere ver sus cartas en el ListView
+     */
     public void rellenarList(Mazo m){
 
         ConnSQLiteHelper c = new ConnSQLiteHelper(this);
         mazoListArray = c.consultarCartasDeMazo(m.getId());
-        mazoAdapterListView = new ArrayAdapter<Carta>(this, android.R.layout.simple_list_item_1, mazoListArray);
+        mazoAdapterListView = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mazoListArray);
         mazoListView.setAdapter(mazoAdapterListView);
 
     }
 
+    /**
+     * Botón que lleva a la actividad para las acciones sobre los mazos.
+     * @param view
+     */
+    public void AjusteMazoButtonPressed(View view){
+        Intent intent = new Intent(this, InsertarMazo.class);
+        startActivity(intent);
+    }
+
+
+
+
+    //SECUNDARIO
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
-
-    public void AjusteMazoButtonPressed(View view){
-        Intent intent = new Intent(this, InsertarMazo.class);
-        startActivity(intent);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -172,7 +201,7 @@ public class MazosActivity extends AppCompatActivity
             finish();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
